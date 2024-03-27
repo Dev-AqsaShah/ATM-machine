245f1b42e20271ad8344f83a53a0d1c15ed7e0a5
aqsashah <159179853+Dev-AqsaShah@users.noreply.github.com>
2024-03-27 22:16:23 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,39 @@
+import inquirer from "inquirer";
+let myBalance = 10000; //Dollar
+let myPin = 1234;
+let pinAnswer = await inquirer.prompt([
+    {
+        name: "pin",
+        message: "enter your pin",
+        type: "number"
+    }
+]);
+if (pinAnswer.pin === myPin) {
+    console.log("Correct pin code!!!");
+    let operationAns = await inquirer.prompt([
+        {
+            name: "operation",
+            message: "please select option",
+            type: "list",
+            choices: ["withdraw", "check balance"]
+        }
+    ]);
+    console.log(operationAns);
+    if (operationAns.operation === "withdraw") {
+        let amountAns = await inquirer.prompt([
+            {
+                name: "amount",
+                message: "enter your amount",
+                type: "number"
+            }
+        ]);
+        myBalance -= amountAns.amount;
+        console.log("your remaining balance is: " + myBalance);
+    }
+    else if (operationAns.operation === "check balance") {
+        console.log("your balance is:" + myBalance);
+    }
+}
+else {
+    console.log("incorrect pin number");
+}
